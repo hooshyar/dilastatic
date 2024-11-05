dc8ce69563c5800ccc5de7034279ba5445670d26
Hooshyar <hooseyr@gmail.com>
2024-11-05 16:00:10 +0300

    update deploy

--- replace_text.py
+++ replace_text.py
@@ -22,7 +22,7 @@ replacements = {
     'dilacode': 'datacode',
     'wp-': 'dc-',
     'elementor': 'dilamentor',
-    'wp-content': 'dc-content',
+    'wp-content': 'content',
 }
 
 # Replace text in files
