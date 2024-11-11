b72af08eb9c385181dacb52f8a4e5b2a54de91d2
Hooshyar <hooseyr@gmail.com>
2024-11-11 22:05:17 +0300

    updated

--- replace_text.py
+++ replace_text.py
@@ -25,13 +25,29 @@ def rename_item(path, replacements):
     new_path = path
     for old, new in replacements.items():
         new_path = new_path.replace(old, new)
-    if new_path != path and not os.path.exists(new_path):
-        shutil.move(path, new_path)
-        return new_path
-    return path
+    if new_path != path:
+        if os.path.exists(new_path):
+            if os.path.isdir(new_path):
+                # Merge directories
+                for item in os.listdir(path):
+                    s = os.path.join(path, item)
+                    d = os.path.join(new_path, item)
+                    if os.path.isdir(s):
+                        shutil.copytree(s, d, dirs_exist_ok=True)
+                    else:
+                        shutil.copy2(s, d)
+                shutil.rmtree(path)
+            else:
+                # Merge files by appending content
+                with open(new_path, 'a', encoding='utf-8') as new_file:
+                    with open(path, 'r', encoding='utf-8') as old_file:
+                        new_file.write(old_file.read())
+                os.remove(path)
+        else:
+            shutil.move(path, new_path)
+    return new_path
 
 def process_directory(root_dir, replacements):
-    # Walk the directory tree and replace text in files
     for dirpath, dirnames, filenames in os.walk(root_dir):
         # Skip the wp-content directory if it's in the root
         if 'wp-content' in dirnames and dirpath == root_dir:
